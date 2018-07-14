d39670e625740bd673df7d4cdbe197ebd24baaa0
Zeyu Chen <xinyifly@live.cn>
2018-07-14 20:21:03 +0800

    feat(account-buff): add account-buff

--- src/client/command/CommandsExecutor.java
+++ src/client/command/CommandsExecutor.java
@@ -193,6 +193,7 @@ public class CommandsExecutor {
         addCommand("int", StatIntCommand.class);
         addCommand("luk", StatLukCommand.class);
         addCommand("enableauth", EnableAuthCommand.class);
+        addCommand("abuff", ABuffCommand.class);
         
         commandsNameDesc.add(levelCommandsCursor);
     }
