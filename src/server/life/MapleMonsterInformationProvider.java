71ed331c01e69ec7b32e43dd6c5344742db777d4
Zeyu Chen <xinyifly@live.cn>
2019-06-14 11:29:46 +0800

    feat(drop): add boss nx drop

--- src/server/life/MapleMonsterInformationProvider.java
+++ src/server/life/MapleMonsterInformationProvider.java
@@ -217,6 +217,19 @@ public class MapleMonsterInformationProvider {
                 return ret;
             }
         }
+
+        // BOSS NX
+        if (isBossFinal(monsterId)) {
+            int monsterLevel = MapleLifeFactory.getMonster(monsterId).getLevel();
+            int cardChance = Math.min(Math.max(monsterLevel, 20), 100) * 10000;
+            ret.add(new MonsterDropEntry(4031865, cardChance, 1, 2, (short) 0)); // 100 NX
+            ret.add(new MonsterDropEntry(4031866, cardChance, 1, 2, (short) 0)); // 250 NX
+            if (monsterLevel >= 100) {
+                int leafChance = Math.min(Math.max(monsterLevel - 100, 6), 50) * 20000;
+                ret.add(new MonsterDropEntry(4001126, leafChance, 1, 1, (short) 0)); // Maple Leaf
+            }
+        }
+
         drops.put(monsterId, ret);
         return ret;
     }
