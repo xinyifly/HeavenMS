3205699c7a93c8817d47f26e588eb72c2d6285de
Zeyu Chen <xinyifly@live.cn>
2019-09-03 22:40:23 +0800

    feat(shop): selling mesos to 1/5

--- src/server/MapleShop.java
+++ src/server/MapleShop.java
@@ -198,7 +198,7 @@ public class MapleShop {
             MapleInventoryManipulator.removeFromSlot(c, type, (byte) slot, quantity, false);
             
             MapleItemInformationProvider ii = MapleItemInformationProvider.getInstance();
-            int recvMesos = ii.getPrice(item.getItemId(), quantity) / 10;
+            int recvMesos = ii.getPrice(item.getItemId(), quantity) / 5;
             if (recvMesos > 0) {
                 c.getPlayer().gainMeso(recvMesos, false);
             }
