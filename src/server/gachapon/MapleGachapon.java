c8163559117dbd9ea48b817359d30ca3d8cbb989
Zeyu Chen <xinyifly@live.cn>
2020-03-10 14:14:37 +0800

    feat(gachapon): reset

--- src/server/gachapon/MapleGachapon.java
+++ src/server/gachapon/MapleGachapon.java
@@ -39,18 +39,18 @@ public class MapleGachapon {
 	public enum Gachapon {
 
 		GLOBAL(-1, -1, -1, -1, new Global()),
-		HENESYS(9100100, 90, 8, 2, new Henesys()),
-		ELLINIA(9100101, 90, 8, 2, new Ellinia()),
-		PERION(9100102, 90, 8, 2, new Perion()),
-		KERNING_CITY(9100103, 90, 8, 2, new KerningCity()),
-		SLEEPYWOOD(9100104, 90, 8, 2, new Sleepywood()),
-		MUSHROOM_SHRINE(9100105, 90, 8, 2, new MushroomShrine()),
-		SHOWA_SPA_MALE(9100106, 90, 8, 2, new ShowaSpaMale()),
-		SHOWA_SPA_FEMALE(9100107, 90, 8, 2, new ShowaSpaFemale()),
-                LUDIBRIUM(9100108, 90, 8, 2, new Ludibrium()),
-		NEW_LEAF_CITY(9100109, 90, 8, 2, new NewLeafCity()),
-                EL_NATH(9100110, 90, 8, 2, new ElNath()),
-		NAUTILUS_HARBOR(9100117, 90, 8, 2, new NautilusHarbor());
+		HENESYS(9100100, 80, 16, 4, new Henesys()),
+		ELLINIA(9100101, 80, 16, 4, new Ellinia()),
+		PERION(9100102, 80, 16, 4, new Perion()),
+		KERNING_CITY(9100103, 80, 16, 4, new KerningCity()),
+		SLEEPYWOOD(9100104, 80, 16, 4, new Sleepywood()),
+		MUSHROOM_SHRINE(9100105, 80, 16, 4, new MushroomShrine()),
+		SHOWA_SPA_MALE(9100106, 80, 16, 4, new ShowaSpaMale()),
+		SHOWA_SPA_FEMALE(9100107, 80, 16, 4, new ShowaSpaFemale()),
+                LUDIBRIUM(9100108, 80, 16, 4, new Ludibrium()),
+		NEW_LEAF_CITY(9100109, 80, 16, 4, new NewLeafCity()),
+                EL_NATH(9100110, 80, 16, 4, new ElNath()),
+		NAUTILUS_HARBOR(9100117, 80, 16, 4, new NautilusHarbor());
                 
 		private static final Gachapon[] values = Gachapon.values();
 
