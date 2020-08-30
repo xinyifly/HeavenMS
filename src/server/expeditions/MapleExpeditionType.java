b93218133f6afdc5d68f4c92354d965e7a7deb45
Zeyu Chen <xinyifly@live.cn>
2020-08-31 01:37:31 +0800

    chore(expeditions): update min size

--- src/server/expeditions/MapleExpeditionType.java
+++ src/server/expeditions/MapleExpeditionType.java
@@ -31,18 +31,18 @@ import config.YamlConfig;
 
 public enum MapleExpeditionType {
 
-    BALROG_EASY(3, 30, 50, 255, 5),
-    BALROG_NORMAL(6, 30, 50, 255, 5),
-    SCARGA(6, 30, 100, 255, 5),
-    SHOWA(3, 30, 100, 255, 5),
-    ZAKUM(6, 30, 50, 255, 5),
-    HORNTAIL(6, 30, 100, 255, 5),
-    CHAOS_ZAKUM(6, 30, 120, 255, 5),
-    CHAOS_HORNTAIL(6, 30, 120, 255, 5),
+    BALROG_EASY(1, 30, 50, 255, 5),
+    BALROG_NORMAL(1, 30, 50, 255, 5),
+    SCARGA(1, 30, 100, 255, 5),
+    SHOWA(1, 30, 100, 255, 5),
+    ZAKUM(1, 30, 50, 255, 5),
+    HORNTAIL(3, 30, 100, 255, 5),
+    CHAOS_ZAKUM(1, 30, 120, 255, 5),
+    CHAOS_HORNTAIL(3, 30, 120, 255, 5),
     ARIANT(2, 7, 20, 30, 5),
     ARIANT1(2, 7, 20, 30, 5),
     ARIANT2(2, 7, 20, 30, 5),
-    PINKBEAN(6, 30, 120, 255, 5),
+    PINKBEAN(3, 30, 120, 255, 5),
     CWKPQ(6, 30, 90, 255, 5);   // CWKPQ min-level 90, found thanks to Cato
     
     private int minSize;
