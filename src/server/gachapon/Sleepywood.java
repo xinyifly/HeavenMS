a271a81d0271bcc2eb268eccbfa78a63d8ff620d
Zeyu Chen <xinyifly@live.cn>
2021-07-15 01:27:04 +0800

    chore(gachapon): Sleepywood add GMA

--- src/server/gachapon/Sleepywood.java
+++ src/server/gachapon/Sleepywood.java
@@ -9,14 +9,16 @@ public class Sleepywood extends GachaponItems {
     @Override
     public int[] getUncommonItems() {
         return new int[] {
-            2040915, 2040920
+            2040915, 2040920,
+            2040816
         };
     }
 
     @Override
     public int[] getRareItems() {
         return new int[] {
-            2040917, 2040922
+            2040917, 2040922,
+            2040815
         };
     }
 }
